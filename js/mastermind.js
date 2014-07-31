a623fab5764dd99d90cacc70f882d33498d5adb8
Jillian Burgess <jillburg7@gmail.com>
2014-07-31 10:16:07 -0400

    Update mastermind.js

--- js/mastermind.js
+++ js/mastermind.js
@@ -2,3 +2,6 @@ $('#tabs a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 });
+
+$('#tabs a:first').tab('show'); // Select first tab
+$('#tabs a:last').tab('show'); // Select last tab

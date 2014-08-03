01e030625793696915da7ceaad151502b3869f65
Jillian Burgess <jillburg7@gmail.com>
2014-08-03 18:22:41 -0400

    updates to css and overall l&f. Added mastermind instructions

--- js/mastermind.js
+++ js/mastermind.js
@@ -1,7 +1,7 @@
-$('#tabs a').click(function (e) {
+$('#mytabs a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 });
 
-$('#tabs a:first').tab('show'); // Select first tab
-$('#tabs a:last').tab('show'); // Select last tab
+// $('#mytabs a:first').tab('show'); // Select first tab
+// $('#mytabs a:last').tab('show'); // Select last tab
